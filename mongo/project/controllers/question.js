da90b5df7cd1d7e40af379420f21661be3d07128
nese.erkeskin <nese.erkeskin@akgun.com.tr>
2022-06-03 16:59:31 +0300

    *questions

--- mongo/project/controllers/question.js
+++ mongo/project/controllers/question.js
@@ -1,11 +1,120 @@
-const getAllQuestions = (req,res,next) => {
-    res
-    .status(200)
-    .json({
-        success: true
-    });
-};
+const Question = require("../models/Question");
+const CustomError = require('../helpers/error/CustomError');
+const asyncErrorWrapper = require("express-async-handler");
 
+const getAllQuestions = asyncErrorWrapper(async (req, res, next) => {
+    const questions = await Question.find();
+
+    return res.status(200)
+        .json({
+            success: true,
+            data: questions
+        });
+});
+
+const askNewQuestion = asyncErrorWrapper(async (req, res, next) => {
+    const information = req.body;
+
+    const question = await Question.create({
+        ...information,
+        user: req.user.id
+    });
+    res.status(200)
+        .json({
+            success: true,
+            data: question
+        });
+});
+
+const getSingleQuestion = asyncErrorWrapper(async (req, res, next) => {
+    const { id } = req.params;
+
+    const question = await Question.findById(id);
+
+    res.status(200)
+        .json({
+            success: true,
+            data: question
+        });
+});
+
+const editQuestion = asyncErrorWrapper(async (req, res, next) => {
+    const { id } = req.params;
+
+    const { title, content } = req.body;
+
+    let question = await Question.findById(id);
+
+    question.title = title;
+    question.content = content;
+
+    question = await question.save();
+
+    return res.status(200)
+        .json({
+            success: true,
+            data: question
+        });
+});
+
+const deleteQuestion = asyncErrorWrapper(async (req, res, next) => {
+    const { id } = req.params;
+
+    await Question.findByIdAndDelete(id);
+
+    res.status(200)
+        .json({
+            success: true,
+            message: "Question delete operation successful"
+        });
+});
+
+const likeQuestion = asyncErrorWrapper(async (req, res, next) => {
+    const { id } = req.params;
+
+    const question = await Question.findById(id);
+
+    if (question.likes.includes(req.user.id)) {
+        return next(new CustomError("You already liked this question", 400));
+    }
+    question.likes.push(req.user.id);
+
+    await question.save();
+
+    return res.status(200)
+        .json({
+            success: true,
+            data: question
+        });
+});
+
+const undoLikeQuestion = asyncErrorWrapper(async (req, res, next) => {
+    const { id } = req.params;
+
+    const question = await Question.findById(id);
+
+    if (!question.likes.includes(req.user.id)) {
+        return next(new CustomError("You cannot undo like operation for this question", 400));
+    }
+
+    const index = question.likes.indexOf(req.user.id);
+
+    question.likes.splice(index, 1);
+
+    await question.save();
+
+    return res.status(200)
+        .json({
+            success: true,
+            data: question
+        });
+});
 module.exports = {
-    getAllQuestions
-};
+    askNewQuestion,
+    getAllQuestions,
+    getSingleQuestion,
+    editQuestion,
+    deleteQuestion,
+    likeQuestion,
+    undoLikeQuestion
+}
--- mongo/project/routers/question.js
+++ mongo/project/routers/question.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const {getAllQuestions} = require('../controllers/question')
-// api/questions
+const { askNewQuestion, getAllQuestions, getSingleQuestion, editQuestion, deleteQuestion, likeQuestion, undoLikeQuestion } = require('../controllers/question');
+const { checkQuestionExist } = require("../middlewares/database/databaseErrorHelpers");
+const { getAccessToRoute, getQuestionOwnerAccess } = require('../middlewares/authorization/auth');
+
 const router = express.Router();
 
+router.get("/:id/like", [getAccessToRoute, checkQuestionExist], likeQuestion);
+router.get("/:id/undo_like", [getAccessToRoute, checkQuestionExist], undoLikeQuestion);
 router.get("/", getAllQuestions);
-
-
+router.get("/:id", checkQuestionExist, getSingleQuestion);
+router.post("/ask", getAccessToRoute, askNewQuestion);
+router.put("/:id/edit", [getAccessToRoute, checkQuestionExist, getQuestionOwnerAccess], editQuestion);
+router.delete("/:id/delete", [getAccessToRoute, checkQuestionExist, getQuestionOwnerAccess], deleteQuestion);
 
 module.exports = router;
