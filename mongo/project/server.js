beac60406edde36a447637b14b50ca874346a1c3
nese.erkeskin <nese.erkeskin@akgun.com.tr>
2024-12-19 20:22:38 +0300

    *cors

--- mongo/project/server.js
+++ mongo/project/server.js
@@ -4,8 +4,8 @@ const connectDatabase = require("./helpers/database/connectDatabase");
 const routers = require("./routers");
 const customErrorHandler = require("./middlewares/errors/customErrorHandler");
 const path = require("path");
-
-
+const cors = require('cors');
+ 
 //Environment Variables
 dotenv.config({
     path: "./config/env/config.env"
@@ -13,11 +13,21 @@ dotenv.config({
 
 //MongoDb Connection
 
-connectDatabase();
+connectDatabase();  
 
 
 const app = express();
+app.use(cors());
 
+app.use(
+    cors({
+      origin: 'http://localhost:8100', // Ionic uygulamanızın adresi
+      methods: 'GET,POST,PUT,DELETE,OPTIONS',
+      allowedHeaders: 'Content-Type,Authorization', 
+    })
+  );
+
+  
 //Express - Body Middleware
 app.use(express.json());
 
